f30da1a89ee2b246e68fcf487244cd226c134a60
Luis <luis-santiago97@hotmail.com>
2019-03-02 22:23:58 -0500

    variable de entorno MONGO_DB

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // BD
 //====================================
 
-let urlDB = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe' : 'mongodb+srv://cafe-user1:mongodb16@cluster0-dgiyl.mongodb.net/cafe?retryWrites=true';
+let urlDB = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe' : process.env.MONGO_URI;
 
 process.env.URLDB = urlDB;
 
